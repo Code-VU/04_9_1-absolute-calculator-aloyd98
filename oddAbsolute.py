9667d03123d16dfc96b2f07637cd478e720bb487
Austin Loyd <Austin.Loyd@vu.com>
2023-05-03 14:59:42 -0500

    try/except statement

--- oddAbsolute.py
+++ oddAbsolute.py
@@ -1,7 +1,19 @@
 def calculateAbsolute():
     
     # This first line is provided for you
-    in_num  = input("Enter a number: ")
+    in_num  = int(input("Enter a number: "))
+    
+    try:
+        if in_num > 21:
+            diff = (in_num - 21) * 2
+
+        else:
+            diff = (in_num - 21) * -1
+    except:
+        print('Please enter a whole number.')
+
+    print('Result:', diff) 
+    
     # end assignment
 
 ## If you want to test locally run > python payCalculator.py
